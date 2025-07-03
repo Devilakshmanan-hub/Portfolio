ecb87b848bf0f4985b67a314e2c9a55538e42ccb
Devilakshmanan-hub <lshyamala1994@gmail.com>
2025-07-03 10:16:48 +0530

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
-import React from 'react';
-import './App.css'
+ import React from 'react';
+import './App.css';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import { useEffect } from 'react';
 import Header from './Components/Header';
 import About from './Components/About';
 import Skills from './Components/Skills';
 import Projects from './Components/Projects';
 import Contact from './Components/Contact';
 import Footer from './Components/Footer';
+import Navbar from './Components/Navbar';
 function App() {
-  
+         useEffect(() => {
+    AOS.init({ duration: 1000 });
+  }, []);
+       
  return (
     <>
+       <Navbar />
        <Header />
        <About />
        <Skills />
